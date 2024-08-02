34acf69455b3b463455a3f760fdfbc99fa2dd30d
CaioBalaniuk <135898371+CaioBalaniuk@users.noreply.github.com>
2024-08-02 19:48:06 -0300

    Update modulos.h

--- modulos.h
+++ modulos.h
@@ -36,6 +36,8 @@ public:
 
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
+//Declaração Modulo de serviço de investimentos
 class MSI : public ISI {
 private:
     RP *repositorio_p;
@@ -52,7 +54,7 @@ public:
     bool atualizar_dados_p(string, int, string, string) override;
     bool excluir_ps(string, int) override;
 };
-
+//Declaração Modulo de apresentação de investimentos
 class MAI : public IAI {
 private:
     MSI* servico = new MSI;
@@ -70,5 +72,86 @@ public:
     void executar() override;
     void sair() override;
 };
+
+/**
+ * @class MSA
+ * @brief Implementação da Interface de Serviço de Autenticação
+ *
+ * Esta classe implementa a interface ISA e é responsável por buscar o CPF e verificar a senha no repositório de contas.
+ */
+
+class MSA : public ISA {
+private:
+    RC* repositorio;  /**< Instância do repositório de contas */
+
+public:
+    /**
+     * @brief Busca CPF no repositório de contas.
+     * @param cpf CPF do usuário.
+     * @return true se o CPF está no repositório de contas, false caso contrário.
+     */
+    bool busca_CPF(string cpf) override;
+
+    /**
+     * @brief Verifica se a senha é correspondente com o CPF digitado.
+     * @param cpf CPF do usuário.
+     * @param senha Senha do usuário.
+     * @return true se a autenticação for bem-sucedida, false caso contrário.
+     */
+    bool verifica_senha(string cpf, string senha) override;
+};
+
+/**
+ * @class MAA
+ * @brief Implementação da Interface de Autenticação do Usuário
+ *
+ * Esta classe implementa a interface IAA e é responsável por receber e armazenar o CPF e a senha do usuário.
+ */
+
+class MAA : public IAA {
+private:
+    string cpf;  /**< CPF do usuário */
+    string senha; /**< Senha do usuário */
+    MSA *servicoAutenticacao = new MSA;/**< Ponteiro para o serviço de autenticação */
+
+public:
+    /**
+     * @brief Solicita o CPF do usuário.
+     */
+    void getCPF() override;
+
+    /**
+     * @brief Solicita a senha do usuário.
+     */
+    void getSenha() override;
+
+    /**
+     * @brief Retira o usuário da autenticação.
+     */
+    void sair_autenticacao() override;
+
+    /**
+     * @brief Retorna o CPF do usuário.
+     * @return CPF do usuário.
+     */
+    string getCpf() const;
+
+    /**
+     * @brief Retorna a senha do usuário.
+     * @return Senha do usuário.
+     */
+    string getSenha() const;
+
+    /**
+     * @brief Inicia o processo de autenticação.
+     */
+    void autenticar();
+
+    /**
+    *@brief Executa as funcionalidades da classe
+    */
+    void executar();
+};
+
 
 #endif // MODULOS_H_INCLUDED
